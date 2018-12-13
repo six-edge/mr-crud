f121ac283d01a47f455530bf45133bfa60601013
Alex <alex@sixedge.com>
2018-12-13 22:13:09 +0100

    Spiced up vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,51 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span class="font-weight-light">🙂 Mr. CRUD</span>
-      </v-toolbar-title>
+  <v-app id="inspire" dark>
+    <v-navigation-drawer v-model="drawer" clipped fixed app>
+      <v-list dense>
+        <v-list-tile @click="log('Home')">
+          <v-list-tile-action>
+            <v-icon>dashboard</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title><router-link to="/">Home</router-link></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="log('Login')">
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title><router-link to="/login">Login</router-link></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title><span class="font-weight-light">😎 Mr. CRUD</span></v-toolbar-title>
       <v-spacer></v-spacer>
-
-      <!-- Router Links -->
-      <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/login">Login</router-link>
-      </div>
-      
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>Link One</v-btn>
+        <v-btn flat>Link Two</v-btn>
+        <v-btn flat>Link Three</v-btn>
+      </v-toolbar-items>
     </v-toolbar>
-
     <v-content>
-      <router-view/> <!-- Router Target -->
-    </v-content>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+          <v-flex shrink>
+            
+            <v-content>
+              <router-view/> <!-- Router Target -->
+            </v-content>
 
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer app fixed>
+      <span>&copy; 2017</span>
+    </v-footer>
   </v-app>
 </template>
 
@@ -25,9 +53,13 @@
 
 export default {
   name: 'App',
-  data () {
-    return {
-      //
+  data: () => ({
+    drawer: true
+  }),
+  methods: {
+    log(msg) {
+      // eslint-disable-next-line
+      console.log(msg)
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/vuetify'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: "#FF6D00",
+    secondary: "#E65100",
+    accent: "#FF9100",
+    error: "#BF360C",
+    warning: "#FDD835",
+    info: "#607D8B",
+    success: "#33691E"
+  }
+})
 
 new Vue({
   router,
